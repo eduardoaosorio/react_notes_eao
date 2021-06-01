21660f2064fb5ae5663bc1fa2862e696b9826692
Eduardo Osorio <eduardoaosorio95@gmail.com>
2021-05-31 23:39:24 -0500

    Create Notes and Note components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 
 import Header from "./components/Header";
+import Notes from "./components/Notes";
 
 function App() {
   return (
     <div className="app-container">
       <Header />
+      <Notes />
     </div>
   );
 }
---
+++ src/components/Note.js
@@ -0,0 +1,23 @@
+import "./Note.css";
+
+import sprite from "../sprite.svg";
+
+function Note(props) {
+  return (
+    <div className="note">
+      <div className="note__actions-box">
+        <svg className="note__icon--edit">
+          <use href={sprite + "#edit"} />
+        </svg>
+        <svg className="note__icon--delete">
+          <use href={sprite + "#delete"} />
+        </svg>
+      </div>
+      <h1 className="note__title">Example Title</h1>
+      <p className="note__text scroll">{props.text}</p>
+      <p className="note__timestamp">Created on 01/01/2000 at 2:00pm</p>
+    </div>
+  );
+}
+
+export default Note;
